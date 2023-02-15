a6082cb6f12b4c4dd31eb0d91790022cbea8be0a
sharpiro <dsharpbb09@gmail.com>
2023-02-15 12:43:28 -0500

    add pst display name

--- PSTParse/MessageLayer/MailStore.cs
+++ PSTParse/MessageLayer/MailStore.cs
@@ -1,4 +1,7 @@
-﻿using PSTParse.ListsTablesPropertiesLayer;
+﻿#nullable enable
+
+using System.Text;
+using PSTParse.ListsTablesPropertiesLayer;
 using PSTParse.NodeDatabaseLayer;
 
 namespace PSTParse.MessageLayer
@@ -8,12 +11,18 @@ namespace PSTParse.MessageLayer
         private PropertyContext _pc;
 
         public EntryID RootFolder { get; }
+        public string? DisplayName { get; }
 
         public MailStore(PSTFile pst)
         {
             _pc = new PropertyContext(SpecialNIDs.NID_MESSAGE_STORE, pst);
-            //RootFolder = new EntryID(_pc.BTH.GetExchangeProperties()[MessageProperty.RootFolder].Data);
             RootFolder = new EntryID(_pc.Properties[MessageProperty.RootFolder].Data);
+
+            _pc.Properties.TryGetValue(MessageProperty.DisplayName, out ExchangeProperty? displayProp);
+            if (displayProp?.Data != null)
+            {
+                DisplayName = Encoding.Unicode.GetString(displayProp.Data);
+            }
         }
     }
 }
